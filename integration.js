4485a78629593072a1e49a157c7502978e93ae56
Josmar Dias <josmarnet@gmail.com>
2015-03-23 16:26:05 -0300

    start button and integration init

--- integration.js
+++ integration.js
@@ -1,3 +1,50 @@
-(function () {
+var Integration = (function () {
+  Lib = {};
 
+  var iframeId = "integration";
+
+  var _restore = {
+    body: {}
+  };
+
+  var restore = function () {
+    for (i in _restore.body) {
+      document.body[i] = _restore.body[i];
+      delete _restore.body[i];
+    }
+    return this;
+  };
+
+  Lib.init = function (accessId) {
+    var iframe = document.createElement("iframe");
+    var props = {
+      scrolling: "no",
+      id: iframeId,
+      src: "/iframe?accessId=" + accessId,
+      frameborder: "0",
+      allowtransparency: "true"
+    };
+    for (i in props) {
+      iframe[i] = props[i];
+    }
+
+    _restore.body.overflow = document.body.overflow;
+    document.body.overflow = "hidden";
+    document.body.appendChild(iframe);
+
+    return this;
+  };
+
+  Lib.close = function () {
+    restore();
+    var iframe = document.getElementById(iframeId);
+    var parent = iframe.parentNode;
+    if(!parent) {
+      return this;
+    }
+    parent.removeChild(iframe);
+    return this;
+  }
+
+  return Lib;
 })();
