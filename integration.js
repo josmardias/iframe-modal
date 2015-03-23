5d9c836cf5d6a21e1654b4541d6061340b31998e
Josmar Dias <josmarnet@gmail.com>
2015-03-23 17:04:13 -0300

    PageState add class to body

--- integration.js
+++ integration.js
@@ -5,21 +5,54 @@ var Integration = (function () {
 
   var PageState = (function (argument) {
     var restoreObj = {
-      body: {}
+      bodyProp: {},
+      bodyClass: [],
+      eventListener: []
     };
 
     var _PageState = {};
 
     _PageState.changeBodyProp = function (key, value) {
-      restoreObj.body.key = document.body.value;
-      document.body.key = value;
+      restoreObj.bodyProp[key] = document.body.value;
+      document.body[key] = value;
+    };
+
+    _PageState.addBodyClass = function (value) {
+      var elementClassList = document.body.classList;
+      if (elementClassList.contains(value)) {
+        return;
+      }
+      restoreObj.bodyClass.push(value);
+      elementClassList.add(value);
+    };
+
+    _PageState.addEventListener = function (el, eventName, callback) {
+      el.addEventListener("eventName", callback);
+      restoreObj.eventListener.push({
+        el: el, callback: callback
+      });
     };
 
     _PageState.restore = function () {
-      for (i in restoreObj.body) {
-        document.body[i] = restoreObj.body[i];
-        delete restoreObj.body[i];
+      var i;
+      var listener;
+      var bodyClass;
+
+      for (i in restoreObj.bodyProp) {
+        document.body[i] = restoreObj.bodyProp[i];
+        delete restoreObj.bodyProp[i];
       }
+
+      for (i in restoreObj.bodyClass) {
+        bodyClass = restoreObj.bodyClass.pop();
+        document.body.classList.remove(bodyClass);
+      }
+
+      for (i in restoreObj.eventListener) {
+        listener = restoreObj.eventListener.pop();
+        listener.el.removeEventListener(listener.callback);
+      }
+
       return this;
     };
 
@@ -41,6 +74,7 @@ var Integration = (function () {
     }
 
     PageState.changeBodyProp("overflow", "hidden");
+    PageState.addBodyClass("integration-noscroll");
     document.body.appendChild(iframe);
 
     return this;
